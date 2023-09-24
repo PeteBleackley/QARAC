948988cc26ceb7093c9caad181e3d6961e50bba7
PeteBleackley <peter.bleackley@playfultechnology.co.uk>
2023-09-24 11:59:28 +0100

    Broadcasting dot products

--- qarac/models/layers/GlobalAttentionPoolingHead.py
+++ qarac/models/layers/GlobalAttentionPoolingHead.py
@@ -9,6 +9,10 @@ Created on Tue Sep  5 07:32:55 2023
 import keras
 import tensorflow
 
+@tensorflow.function
+def dot_prod(x,y):
+    return tensorflow.tensordot(x,y,axes=1)
+
 class GlobalAttentionPoolingHead(keras.layers.Layer):
     
     def __init__(self):
@@ -44,7 +48,11 @@ class GlobalAttentionPoolingHead(keras.layers.Layer):
         self.local_projection = self.add_weight('local projection',shape=(width,width))
         self.built=True
     
-    
+    @tensorflow.function
+    def project_local(self,X):
+        return tensorflow.tensordot(X,
+                                    self.local_projection,
+                                    axes=1)
         
     def call(self,X,training=None):
         """
@@ -68,9 +76,8 @@ class GlobalAttentionPoolingHead(keras.layers.Layer):
                                                                   self.global_projection,
                                                                  axes=1),
                                             axis=1)
-        lp = tensorflow.linalg.l2_normalize(tensorflow.tensordot(X,
-                                                                 self.local_projection,
-                                                                 axes=1),
+        lp = tensorflow.linalg.l2_normalize(tensorflow.vectorized_map(self.project_local,
+                                                                      X),
                                             axis=2)
         attention = tensorflow.tensordot(lp,gp,axes=1)
         return tensorflow.reduce_sum(attention *X,

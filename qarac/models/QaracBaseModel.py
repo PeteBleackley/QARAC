2f6dc269dcdc21161d16add8e5b12d605248566f
PeteBleackley <peter.bleackley@playfultechnology.co.uk>
2023-10-09 10:29:29 +0100

    Problems with config

--- qarac/models/QaracBaseModel.py
+++ qarac/models/QaracBaseModel.py
@@ -10,7 +10,7 @@ import transformers
 
 class QaracBaseModel(transformers.PreTrainedModel):
     """Base class for Qarac Models. Provided config_class"""
-    config_class = transformers.RobertaConfig
+    config_class = transformers.PretrainedConfig
     
     def __init__(self,config,*inputs,**kwargs):
         super(QaracBaseModel,self).__init__(config,*inputs,**kwargs)
